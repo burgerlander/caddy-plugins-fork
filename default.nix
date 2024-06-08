931b4b8d3e61913778ed1bed002eb150171761ac
Brian Picciano <me@mediocregopher.com>
2024-06-08 20:47:07 +0200

    Initial commit

---
+++ default.nix
@@ -0,0 +1,17 @@
+{
+  pkgsSrc ? builtins.fetchTarball {
+    name = "nixpkgs-24.05";
+    url = "https://github.com/NixOS/nixpkgs/archive/5646423bfac84ec68dfc60f2a322e627ef0d6a95.tar.gz";
+    sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  },
+}: let
+    pkgs = (import pkgsSrc) {};
+in {
+    shell = pkgs.mkShell {
+        name = "project-shell";
+        buildInputs = [
+          #pkgs.go
+          #pkgs.golangci-lint
+        ];
+    };
+}
